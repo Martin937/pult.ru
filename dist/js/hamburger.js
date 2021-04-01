4e60fc2e92b1180c236e21de54b0f100092ba0ef
Martin937 <Martin937@yandex.ru>
2021-04-01 20:08:27 +0300

    finish

---
+++ dist/js/hamburger.js
@@ -0,0 +1,18 @@
+"use strict";
+window.addEventListener("DOMContentLoaded", () => {
+  const nav = document.querySelector(".nav"),
+    navLink = document.querySelectorAll(".nav__link"),
+    hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("hamburger_active");
+    nav.classList.toggle("nav_active");
+  });
+
+  navLink.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburger.classList.toggle("hamburger_active");
+      nav.classList.toggle("nav_active");
+    });
+  });
+});
